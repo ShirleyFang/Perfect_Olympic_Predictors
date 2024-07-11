b309fd9c62517e47291a3de19f125ea92d02047d
Joseph Song <song.josephmin@gmail.com>
2024-07-11 15:25:54 -0700

    files and gitignore

--- naive_bayes_model.py
+++ naive_bayes_model.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 def main():
-    teams_file = "./Teams.xlsx"
-    medals_file = "./Medals.xlsx"
+    teams_file = "./olympic2021data/Teams.xlsx"
+    medals_file = "./olympic2021data/Medals.xlsx"
 
     # Cleaning teams dataset
     df_teams = pd.read_excel(teams_file, sheet_name="Details")
@@ -12,12 +12,18 @@ def main():
 
     # Cleaning medals dataset
     df_medals = pd.read_excel(medals_file, sheet_name="Details")
-    df_medals = df_medals.drop(['Rank', 'Rank by Total'], axis=1)
+    df_medals = df_medals.drop(['Rank', 'Rank by Total', 'Gold', 'Silver', "Bronze"], axis=1)
 
     # Calculating probs
     # ## Probability that a team wins a medal
-    country_teams = df_teams.groupby("NOC").count()
-    print(country_teams)
+    country_teams = df_teams.groupby('NOC').count().rename(columns={'Discipline': 'Teams'})
+
+    # How to get info from each dataset
+    # print(country_teams.loc['Argentina', 'Teams'])
+    # print(df_medals.loc[df_medals['Team/NOC'] == 'Japan', 'Total'].values[0])
+
+    
+
 
 
 main()
