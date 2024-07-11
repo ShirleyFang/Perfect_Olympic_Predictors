9fd509d12d52ee5833f3af0ff508da75b226effa
Joseph Song <song.josephmin@gmail.com>
2024-07-11 14:42:23 -0700

    groupby

--- naive_bayes_model.py
+++ naive_bayes_model.py
@@ -13,8 +13,11 @@ def main():
     # Cleaning medals dataset
     df_medals = pd.read_excel(medals_file, sheet_name="Details")
     df_medals = df_medals.drop(['Rank', 'Rank by Total'], axis=1)
-    
-    # 
+
+    # Calculating probs
+    # ## Probability that a team wins a medal
+    country_teams = df_teams.groupby("NOC").count()
+    print(country_teams)
 
 
 main()
